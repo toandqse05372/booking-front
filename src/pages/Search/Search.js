ac5964ab700d952c398c2d6eb5932cf5aa2bc99a
heuhiu <hieuvuong3562@gmail.com>
2020-05-31 10:23:48 +0700

    [Search funtion] added

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -12,9 +12,12 @@ class Search extends Component {
         super(props);
         this.state = {
             txtParkName: '',
-            txtCityID: ''
+            txtCityID: '',
+            searchList: [],
         }
     }
+
+    //when fill on the form all value and name stored
     onChange = (e) => {
         var target = e.target;
         var name = target.name;
@@ -23,82 +26,86 @@ class Search extends Component {
             [name]: value
         })
     }
-    
+
+    // send request to server to get Park information by Name
     onSubmitSearch = (e) => {
-            e.preventDefault();
-            var { txtParkName, txtCityID } = this.state;
-            // var { history } = this.props;
-            // callApi('park/searchName', 'GET', {
-            //     Name: txtParkName,
-            //     // city_id : txtCityID,
-            axios.get('http://localhost:8090/park/searchName', {
-                params: {
-                  name: txtParkName,
-                    //page = 1 Temp 
-                  page: 1,
-                    //limit = 1 Temp 
-                    limit: 10,
-                }
-              }).then(res => {
-                console.log(res);
-                // history.push("/");
+        e.preventDefault();
+        var { txtParkName, txtCityID, searchList } = this.state;
+        // callApi('park/searchName', 'GET', {
+        //     Name: txtParkName,
+        //     // city_id : txtCityID,
+        axios.get('http://localhost:8090/park/searchName', {
+            params: {
+                name: txtParkName,
+                //page = 1 Temp 
+                page: 1,
+                //limit = 10 Temp 
+                limit: 10,
+            }
+        }).then(res => {
+                console.log(res.data.listResult);
+                this.setState({
+                    searchList: res.data.listResult
+                })
             }).catch(function (error) {
                 console.log(error.response);
-                // if (error.response) {
-                //     // Request made and server responded
-                //     console.log(error.response.data);
-                //     if (error.response.data.toString() === 'WRONG_USERNAME_PASSWORD') {
-                //         alert(t('Error.WRONG_USERNAME_PASSWORD'));
-                //     }
-                    //   history.push("/");
-
-                // }
             });
     }
-    render() {
-        const items = Information.map((data, index) => {
-            
-            return (
-                
-                <div key={data.name}>
-                    <div>
-                        <ul>
-                            <li>
-                                <span >{data.name}</span>
-                                <span >{data.age}</span>
-                                <span >{data.country}</span>
-                                <span >{data.parkname}</span>
-                            </li>
-                        </ul>
+
+    // get list by map & return by html tag
+    showSearchList = (searchList) => {
+        var result = null;
+        if (searchList.length > 0) {
+            result = searchList.map((data, index) => {
+                return (
+                    //specifire key for each data
+                    <div key={data.id}>
+                        <div>
+                            <ul>
+                                <li>
+                                    <span >{data.cityName}</span>
+                                    <br></br>
+                                    <span >{data.description}</span>
+                                    <br></br>
+                                    <span >{data.name}</span>
+                                    <br></br>
+                                    <span >{data.phoneNumber}</span>
+                                    <br></br>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            )
-        })
-        const { txtParkName } = this.state;
+                );
+            });
+        }
+        return result;
+    }
+
+
+    render() {
+        const { txtParkName, searchList } = this.state;
         return (
             <Container fluid>
                 <Form onSubmit={this.onSubmitSearch} >
-                <InputGroup>
-                    <FormControl
-                        type="text"
-                        // placeholder="Search"
-                        name="txtParkName"
-                        value={txtParkName}
-                        // className="mr-sm-2" 
-                        onChange = {this.onChange}
-                    />
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search"
+                            name="txtParkName"
+                            value={txtParkName}
+                            onChange={this.onChange}
+                        />
                     </InputGroup>
-                    <Button 
+                    <Button
                         type="Submit"
                         variant="outline-success">
                         Search
                     </Button>
                 </Form>
-        {/* { items } */}
+                {this.showSearchList(searchList)}
             </Container >
         );
     }
 }
 
-// export default HomePage;
 export default withTranslation()(Search);
